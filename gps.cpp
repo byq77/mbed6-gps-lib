cec4b964d14187e79c3120474ffcd8a2fd2335cc
byq77 <byq77@hotmail.com>
2023-05-09 16:59:17 +0200

    fix

--- gps.cpp
+++ gps.cpp
@@ -42,7 +42,6 @@ bool GPS::update()
                &alt, &geoid, &age_of_diff, &diff_ref_station, &checksum) >= 1)
     {
         _state = InputState::LOOK_FOR_DOLLA;
-        _message_in_buffer = true;
         _data_read = 0;
         fix = static_cast<FixType>(fix_raw);
         if(fix == FixType::FIX_NOT_AVAILABLE)
@@ -102,14 +101,17 @@ void GPS::readSentence()
         {
             case InputState::LOOK_FOR_DOLLA:
                 if (ch == '$')
+                {
                     _state = InputState::READING_MESSAGE;
                     _message_in_buffer = false;
+                }
                 break;
             case InputState::READING_MESSAGE:
                 if(ch == '\r' || ch == '\n')
                 {
                     *ptr = 0;
                     _state = InputState::MESSAGE_READ;
+                    _message_in_buffer = true;
                     return;
                 }
                 else
